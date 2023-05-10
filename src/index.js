1278229afbba6fa86c5a6a9214cf6b2f8c018339
Mauri-salazar <maurisa2004@gmail.com>
2023-05-10 11:42:46 -0300

    firts commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import './styles/global.scss';
+
+const root = ReactDOM.createRoot(document.getElementById('app'));
+
+root.render(<App />);
